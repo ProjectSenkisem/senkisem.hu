42c92e34b3d1fa33a364eb520df561a1c5410c2f
Bellér Zoltán Ezra <zoleeka12345@gmail.com>
2026-02-03 15:33:43 +0100

    hero no gsap

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -112,7 +112,7 @@ const Features = () => {
               </>
             }
             description="Minden darab egy történet. Minden történet egy élmény. Nem logókat viselsz – hanem üzeneteket."
-            imageStyle={{ objectPosition: "center 60%" }}
+            imageStyle={{ objectPosition: "center -30%" }}
           />
         </BentoTilt>
 
